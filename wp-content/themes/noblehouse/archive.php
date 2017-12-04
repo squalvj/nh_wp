ac81e5f8bac4f6ee1eda5e07d3df43e41752fb81
Squalvj <squalvj@gmail.com>
2017-12-04 17:16:23 +0700

    added carousel & layout desktop archive

--- wp-content/themes/noblehouse/archive.php
+++ wp-content/themes/noblehouse/archive.php
@@ -1,17 +1,87 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php 
-if ( is_day() ) { printf( __( 'Daily Archives: %s', 'blankslate' ), get_the_time( get_option( 'date_format' ) ) ); }
-elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'blankslate' ), get_the_time( 'F Y' ) ); }
-elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'blankslate' ), get_the_time( 'Y' ) ); }
-else { _e( 'Archives', 'blankslate' ); }
-?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/rail-custom.css">
+<div class="background-b">
+	<?php get_template_part( 'nav/nav', 'side' ); ?>
+	<div class="container">
+	    <div class="wrapper-header-accordion">
+	        <h5>News & Promotion</h5>
+	    </div>
+	    <div class="wrapper-archive">
+			<div class="wrapper-year scrollbar-rail">
+				<div class="group">
+					<ul>
+						<a class="year" href="#">
+							2017
+						</a>
+						<li> <a href="#"> January </a> </li>
+						<li> <a href="#"> February </a> </li>
+						<li> <a href="#"> March </a> </li>
+						<li> <a href="#"> April </a> </li>
+						<li> <a href="#"> May </a> </li>
+						<li> 
+							<a href="#" class="active"> June </a> 
+							<ul class="child">
+								<li> <a class="active" href="#">A news about Noble House </a> </li>
+								<li> <a href="#">Lorem Ipsum Dolor Sit Amet Cocuses Mundos Aegleious Kantortos </a> </li>
+								<li> <a href="#">A news about Noble House </a> </li>
+								<li> <a href="#">A news about Noble House </a> </li>
+							</ul>
+						</li>
+						<li> <a href="#"> July </a> </li>
+						<li> <a href="#"> August </a>
+						<li> <a href="#"> September </a> </li>
+						<li> <a href="#"> October </a> </li>
+						<li> <a href="#"> November </a> </li>
+						<li> <a href="#"> December </a> </li>
+					</li>
+				</div>
+				<div class="group">
+					<ul>
+						<a class="year" href="#">
+							2016
+						</a>
+						<li> <a href="#"> January </a> </li>
+						<li> <a href="#"> February </a> </li>
+						<li> <a href="#"> March </a> </li>
+						<li> <a href="#"> April </a> </li>
+						<li> <a href="#"> May </a> </li>
+						<li> 
+							<a href="#"> June </a> 
+						</li>
+						<li> <a href="#"> July </a> </li>
+						<li> <a href="#"> August </a>
+						<li> <a href="#"> September </a> </li>
+						<li> <a href="#"> October </a> </li>
+						<li> <a href="#"> November </a> </li>
+						<li> <a href="#"> December </a> </li>
+					</li>
+				</div>
+			</div>
+			<div class="wrapper-article scrollbar-rail">
+				<h2>Nikolas Weinstein’s Jakarta  project pushes scale, and his design and installation team, to the limit</h2>
+				<p>an Francisco-based glass sculptor and designer Nikolas Weinstein is not completely comfortable with any of those terms. At least he said as much in a feature article in the Spring 2011 edition of the print magazine (GLASS #122). Weinstein's singular focus is to create large-scale, unorthodox glass sculptures that exist in conversation with the architectural spaces that house them. They also happen to do things you might never have imagined glass can do. Just before Christmas 2016, Weinstein and his team installed their largest and most complex sculpture yet in the lobby of a Jakarta, Indonesia, office building. Weinstein had been asked by the developers of the Noble House, a premier office tower in downtown Jakarta, to design something that would set their new building apart, and the results didn't disappoint.
+				Beyond scale and complexity, the project’s engineering team faced another, more location-specific challenge in the installation process. Indonesia, an archipelagic nation, is prone to frequent and intense seismic activity. In fact, just before the project’s launch, the country’s eastern province an Francisco-based glass sculptor and designer Nikolas Weinstein is not completely comfortable with any of those terms. At least he said as much in a feature article in the Spring 2011 edition of the print magazine (GLASS #122). Weinstein's singular focus is to create large-scale, unorthodox glass sculptures that exist in conversation with the architectural spaces that house them. They also happen to do things you might never have imagined glass can do. Just before Christmas 2016, Weinstein and his team installed their largest and most complex sculpture yet in the lobby of a Jakarta, Indonesia, office building. Weinstein had been asked by the developers of the Noble House, a premier office tower in downtown Jakarta, to design something that would set their new building apart, and the results didn't disappoint.
+				Beyond scale and complexity, the project’s engineering team faced another, more location-specific challenge in the installation process. Indonesia, an archipelagic nation, is prone to frequent and intense seismic activity. In fact, just before the project’s launch, the country’s eastern province an Francisco-based glass sculptor and designer Nikolas Weinstein is not completely comfortable with any of those terms. At least he said as much in a feature article in the Spring 2011 edition of the print magazine (GLASS #122). Weinstein's singular focus is to create large-scale, unorthodox glass sculptures that exist in conversation with the architectural spaces that house them. They also happen to do things you might never have imagined glass can do. Just before Christmas 2016, Weinstein and his team installed their largest and most complex sculpture yet in the lobby of a Jakarta, Indonesia, office building. Weinstein had been asked by the developers of the Noble House, a premier office tower in downtown Jakarta, to design something that would set their new building apart, and the results didn't disappoint.
+				Beyond scale and complexity, the project’s engineering team faced another, more location-specific challenge in the installation process. Indonesia, an archipelagic nation, is prone to frequent and intense seismic activity. In fact, just before the project’s launch, the country’s eastern province </p>
+			</div>
+			<div class="wrapper-gallery-article">
+				<div class="owl-carousel owl-theme">
+					<div class="item">
+						<img src="<?= get_template_directory_uri(); ?>/assets/img/bg-office.jpg">
+					</div>
+					<div class="item">
+						<img src="<?= get_template_directory_uri(); ?>/assets/img/bg-office.jpg">
+					</div>
+					<div class="item">
+						<img src="<?= get_template_directory_uri(); ?>/assets/img/bg-office.jpg">
+					</div>
+					<div class="item">
+						<img src="<?= get_template_directory_uri(); ?>/assets/img/bg-office.jpg">
+					</div>
+				</div>
+			</div>
+	    </div>
+	   </div>
+    <?php get_template_part( 'nav/nav', 'bottom' ); ?>
+</div>
 <?php get_footer(); ?>
